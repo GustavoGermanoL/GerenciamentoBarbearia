fa123d4fa9ecf58c2ee8b6e970ac96d68055e6d3
GustavoGermanoL <gustavinhoglp@gmail.com>
2025-09-25 12:41:02 -0300

    first commit

---
+++ backend/BarbeariaProject/src/main/java/com/Barbearia/BarbeariaProject/BarbeariaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.Barbearia.BarbeariaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarbeariaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarbeariaProjectApplication.class, args);
+	}
+
+}
---
+++ backend/BarbeariaProject/src/test/java/com/Barbearia/BarbeariaProject/BarbeariaProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Barbearia.BarbeariaProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarbeariaProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
